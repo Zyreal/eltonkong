fa7fd779a597d1415c64852a650ea0eb366f31e2
Elton Kong <111011984+Zyreal@users.noreply.github.com>
2022-12-01 01:17:38 -0500

    Added Sanity

---
+++ backend_sanity/schemas/projects.js
@@ -0,0 +1,26 @@
+export default {
+    name: 'projects',
+    title: 'Projects',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string'
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'array',
+            of: [{type: 'block'}]
+        },
+        {
+            name: 'imgUrl',
+            title: 'ImgUrl',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        }
+    ],
+}
